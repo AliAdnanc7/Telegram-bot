8869f9e69b30b4254ac6cc8b48a41965e8f56583
Eugene Nikitin <uchiha659@gmail.com>
2020-08-06 23:52:15 +0300

    puppeteer fix

--- scrapers.js
+++ scrapers.js
@@ -3,10 +3,7 @@ const puppeteer = require('puppeteer')
 async function scrape(url, callback) {
 
     try{
-        const browser = await puppeteer.launch({
-            headless: true,
-            args: ["--no-sandbox"]
-        })
+        const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']})
         const page = await browser.newPage()
         await page.goto(url)
         await callback(page)
